da6966f97ba789f483024eae1d134e2a5afad3cf
Stephan Max <stephan.max@gmail.com>
2018-05-15 17:59:56 +0200

    Inline CSS on build

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
-const cssnano = require('gulp-cssnano')
 const del = require('del')
 const gulp = require('gulp')
+const inline = require('gulp-inline-source')
 const metalsmithBuild = require('./metalsmith.build')
 
-gulp.task('clean', () => del(['build']))
+gulp.task('clean', () => del(['./build']))
 
 gulp.task('metalsmith', () => metalsmithBuild())
 
 gulp.task('metalsmith-watch', ['metalsmith'], () => {
-  gulp.watch(['src/**/*', '!src/assets/'], ['metalsmith'])
+  gulp.watch(['./src/**/*', '!./src/assets/'], ['metalsmith'])
 })
 
 gulp.task('stylesheets', () => {
-  return gulp.src('src/assets/**/*.css').pipe(cssnano()).pipe(gulp.dest('build/assets'))
+  return gulp.src('./src/assets/**/*.css').pipe(gulp.dest('./build/assets'))
 })
 
 gulp.task('stylesheets-watch', ['stylesheets'], () => {
-  gulp.watch('src/assets/**/*.css', ['stylesheets'])
+  gulp.watch('./src/assets/**/*.css', ['stylesheets'])
+})
+
+gulp.task('inline', () => {
+  return gulp.src('./build/**/*.html').pipe(inline({
+    rootpath: './src'
+  })).pipe(gulp.dest('./build'))
 })
