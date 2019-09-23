112aee6ca79132b572ef5d1ba5914a6f43683f3d
Stephan Max <hello@stephanmax.com>
2019-09-23 15:42:51 +0200

    Update to gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,33 @@
-const autoprefixer = require('autoprefixer')
-const del = require('del')
-const gulp = require('gulp')
-const inline = require('gulp-inline-source')
-const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer');
+const del = require('del');
+const gulp = require('gulp');
+const inline = require('gulp-inline-source');
+const postcss = require('gulp-postcss');
 
-const metalsmithBuild = require('./metalsmith.build')
+const metalsmithBuild = require('./metalsmith.build');
 
 gulp.task(
   'clean',
   () => del(['build'])
-)
+);
 
 gulp.task(
   'metalsmith',
-  () => metalsmithBuild()
-)
+  async () => await metalsmithBuild()
+);
 
 gulp.task(
   'metalsmith-watch',
-  ['metalsmith'],
-  () => gulp.watch(['src/**/*', 'layouts/**/*'], ['metalsmith'])
-)
+  gulp.series(
+    'metalsmith',
+    async () => await gulp.watch(
+      ['src/**/*', 'layouts/**/*'],
+      gulp.series('metalsmith')
+    )
+  )
+);
 
-const stylesPath = 'assets/**/*.css'
+const stylesPath = 'assets/**/*.css';
 
 gulp.task(
   'styles',
@@ -34,30 +39,40 @@ gulp.task(
       })
     ]))
     .pipe(gulp.dest('build/assets'))
-)
+);
 
 gulp.task(
   'styles-watch',
-  ['styles'],
-  () => gulp.watch(stylesPath, ['styles'])
-)
+  gulp.series(
+    'styles',
+    () => gulp.watch(
+      stylesPath,
+      gulp.series('styles')
+    )
+  )
+);
 
-const imagesPath = 'assets/**/*.{gif,jpg,png,svg}'
+const imagesPath = 'assets/**/*.{gif,jpg,png,svg}';
 
 gulp.task(
   'images',
   () => gulp.src(imagesPath).pipe(gulp.dest('build/assets'))
-)
+);
 
 gulp.task(
   'images-watch',
-  ['images'],
-  () => gulp.watch(imagesPath, ['images'])
-)
+  gulp.series(
+    'images',
+    () => gulp.watch(
+      imagesPath,
+      gulp.series('images')
+    )
+  )
+);
 
 gulp.task(
   'inline',
   () => gulp.src('build/**/*.html').pipe(inline({
     rootpath: 'build'
   })).pipe(gulp.dest('build'))
-)
+);
